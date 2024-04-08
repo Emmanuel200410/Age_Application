ee7caf6b3aeb49ce711f0c1bfdcabda4898c31b2
Emmanuel200410 <ST10450437@rcconnect.edu.za>
2024-04-08 20:50:25 +0200

    checkbutton

--- app/src/main/java/com/example/age_appication/MainActivity.kt
+++ app/src/main/java/com/example/age_appication/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.age_appication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
         private val famousPeople = listOf(
@@ -14,5 +16,22 @@ class MainActivity : AppCompatActivity() {
             Pair("kobe bryant" ,41),
             Pair("stan lee" ,94),
             Pair("leonardo da vinci" ,67)
+
         )
+        private lateinit var ageInput: EditText
+        private lateinit var resultTextView: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        ageInput = findViewById(R.id.editTextText)
+        resultTextView = findViewById(R.id.text_view)
+        val checkButton: Button = findViewById(R.id.button)
+        val clearButton: Button = findViewById(R.id.button4)
+        checkButton.setOnClickListener {
+            checkAge()
+        }
+        clearButton.setOnClickListener {
+            clearFields()
+
+
 }

6d28366040fab062ace0a45e24ca1ab637591530
Emmanuel200410 <ST10450437@rcconnect.edu.za>
2024-04-08 20:33:21 +0200

    FamousPeople Array

--- app/src/main/java/com/example/age_appication/MainActivity.kt
+++ app/src/main/java/com/example/age_appication/MainActivity.kt
@@ -4,8 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        private val famousPeople = listOf(
+            Pair("Nelson Mandela", 95),
+            Pair("William Shakespeare", 52),
+            Pair("queen elizabeth", 96),
+            Pair("Steve jobbs",56),
+            Pair("pele",82),
+            Pair("maradona" ,60),
+            Pair("kobe bryant" ,41),
+            Pair("stan lee" ,94),
+            Pair("leonardo da vinci" ,67)
+        )
 }

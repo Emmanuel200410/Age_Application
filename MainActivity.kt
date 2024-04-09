8d4e06ea174057f0f23f519922765cda2c57cc0e
Emmanuel200410 <162302164+Emmanuel200410@users.noreply.github.com>
2024-04-09 18:23:57 +0200

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,78 @@
+package com.example.age_appication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    //define famous people and their ages
+        private val famousPeople = listOf(
+            Pair("Nelson Mandela", 95),
+            Pair("William Shakespeare", 52),
+            Pair("queen elizabeth", 96),
+            Pair("Steve jobbs",56),
+            Pair("pele",82),
+            Pair("maradona" ,60),
+            Pair("kobe bryant" ,41),
+            Pair("stan lee" ,94),
+            Pair("leonardo da vinci" ,67)
+
+        )
+    // ui elements
+        private lateinit var ageInput: EditText
+        private lateinit var resultTextView: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        ageInput = findViewById(R.id.editTextText)
+        resultTextView = findViewById(R.id.text_view)
+        val checkButton: Button = findViewById(R.id.button)
+        val clearButton: Button = findViewById(R.id.button4)
+        checkButton.setOnClickListener {
+            checkAge()
+        }
+        clearButton.setOnClickListener {
+            clearFields()
+        }
+    }
+
+
+
+        private fun checkAge() {
+            val ageStr = ageInput.text.toString().trim()
+            if (ageStr.isEmpty()) {
+                resultTextView.text = ("Please enter your age")
+                return
+            }
+            val UsersAge: Int = try {
+                ageStr.toInt()
+            } catch (e: NumberFormatException) {
+                resultTextView.text = ("The age is in an invalid format")
+                return
+            }
+            //validate age range
+            if (UsersAge !in 20..100) {
+                resultTextView.text = "This age is out of range (20-100)";
+                return
+            }
+            //find famous person withh exact age as input
+            for ((name, age) in famousPeople) {
+                if(UsersAge == age ) {
+                    resultTextView.text = ("you are $age the historical figure  $name died at $age")
+                    break
+                }
+                else{
+                    resultTextView.text = "There is no known historical figure who died at that age"
+                }
+            }
+        }
+
+    //method to clear input
+    private fun clearFields() {
+            ageInput.text.clear()
+        }
+
+
+    }
